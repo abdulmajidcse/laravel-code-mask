bd4b264235b25b5b625f167723eceb2922098790
Abdul Majid <abdulmajidcse@gmail.com>
2024-06-29 00:30:31 +0600

    Updated facade details.

--- src/Facades/CodeMaskFacade.php
+++ src/Facades/CodeMaskFacade.php
@@ -4,15 +4,14 @@ namespace Abdulmajidcse\LaravelCodeMask\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string hideString(string $string, int $visibleInStart = 1, int $visibleInEnd = 1, string $maskString = '*')
+ * @method static string generateId(string $string, int $length = 10, string $padString = '0', string $prefix = '')
+ *
+ * @see \Abdulmajidcse\LaravelCodeMask\Services\LaravelCodeMask
+ */
 class CodeMaskFacade extends Facade
 {
-    /**
-     * @method static string hideString(string $string, int $visibleInStart = 1, int $visibleInEnd = 1, string $maskString = '*')
-     * @method static string generateId(string $string, int $length = 10, string $padString = '0', string $prefix = '')
-     *
-     * To get available methods information
-     * @see \Abdulmajidcse\LaravelCodeMask\Services\LaravelCodeMask
-     */
     protected static function getFacadeAccessor()
     {
         return 'abdulmajidcse-laravel-code-mask';
